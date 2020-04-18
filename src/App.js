0008663c4a7d2d1cdfda02d0105897830c70e722
David Chiche <davidchiche@gmail.com>
2020-04-18 23:53:32 +0900

    Created form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      message: "Hello Coding Garden",
+      newTodo: "",
+    };
+  }
+  formSubmitted(event) {
+    event.preventDefault();
+    console.log("form submitted");
+  }
+  newTodoChanged(event) {
+    this.setState({
+      newTodo: event.target.value,
+    });
+    console.log(event.target.value);
+  }
+  render() {
+    return (
+      <div className="App">
+        <h3>{this.state.message}</h3>
+        <form onSubmit={this.formSubmitted}>
+          <label htmlFor="newTodo">New Todo</label>
+          <input
+            onChange={(event) => this.newTodoChanged(event)}
+            id="newTodo"
+            name="newTodo"
+          />
+          <button type="submit">Add Todo</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
