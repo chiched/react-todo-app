d57329803183c5cf20e3e27b078d13ece388d90a
David Chiche <davidchiche@gmail.com>
2020-04-20 22:56:02 +0900

    Deconstructed in different components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import NewTodoForm from "./NewTodoForm";
+import TodoList from "./TodoList";
 import "./App.css";
 
 class App extends Component {
@@ -63,39 +65,17 @@ class App extends Component {
     return (
       <div className="App">
         <h3>{this.state.message}</h3>
-        <form onSubmit={(event) => this.formSubmitted(event)}>
-          <label htmlFor="newTodo">Newest Todo</label>
-          <input
-            onChange={(event) => this.newTodoChanged(event)}
-            id="newTodo"
-            name="newTodo"
-            value={this.state.newTodo}
-          />
-          <button type="submit">Add Todo</button>
-        </form>
+        <NewTodoForm
+          newTodo={this.state.newTodo}
+          formSubmitted={this.formSubmitted.bind(this)}
+          newTodoChanged={this.newTodoChanged.bind(this)}
+        />
         <button onClick={() => this.allDone()}>All done</button>
-        <ul>
-          {this.state.todos.map((todo, index) => {
-            return (
-              <li key={index}>
-                <input
-                  onChange={(event) => this.toggleTodoDone(event, index)}
-                  type="checkbox"
-                  checked={todo.done}
-                />
-                {/* <span
-                  style={{
-                    textDecoration: todo.done ? "line-through" : "inherit",
-                  }}
-                >
-                  {todo.title}
-                </span> */}
-                <span className={todo.done ? "done" : ""}>{todo.title}</span>
-                <button onClick={() => this.removeTodo(index)}>Remove</button>
-              </li>
-            );
-          })}
-        </ul>
+        <TodoList
+          todos={this.state.todos}
+          toggleTodoDone={this.toggleTodoDone.bind(this)}
+          removeTodo={this.removeTodo.bind(this)}
+        />
       </div>
     );
   }
