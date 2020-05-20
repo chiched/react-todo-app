7256deff481f598bf51ed2eb99c00971809c8f09
David Chiche <davidchiche@gmail.com>
2020-05-20 17:59:47 +0900

    Added styling to form NewTodo

--- client/src/NewTodoForm.js
+++ client/src/NewTodoForm.js
@@ -1,16 +1,26 @@
 import React from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCoffee,
+  faPlus,
+  faTrashAlt,
+  faStar,
+  faSignInAlt,
+  faSignOutAlt,
+  faPlusCircle,
+} from "@fortawesome/free-solid-svg-icons";
 const NewTodoForm = (props) => {
   return (
     <form className="add-todo" onSubmit={props.formSubmitted}>
-      new todo
       <input
         onChange={props.newTodoChanged}
         id="newTodo"
         name="newTodo"
         value={props.newTodo}
       />
-      <button type="submit">+</button>
+      <button type="submit">
+        <FontAwesomeIcon className="logout" icon={faPlusCircle} size="lg" />
+      </button>
     </form>
   );
 };
