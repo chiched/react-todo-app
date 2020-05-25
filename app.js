bd7030974ef67448f68c5a1952bfaa706768dd5b
David Chiche <davidchiche@gmail.com>
2020-05-25 23:18:53 +0900

    Changed static path in app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
-app.use(express.static(path.join(__dirname + "./client/build")));
+app.use(express.static(path.join(__dirname + "/client/build")));
 
 app.use("/auth", auth);
 app.use("/api", todo);
